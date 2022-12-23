a06f54bd009ccdf2051a020850ab0a0344c55bcd
rdynxyz <radyan6661@gmail.com>
2022-12-23 10:55:21 +0700

    +db menu

--- src/components/food-menu.vue
+++ src/components/food-menu.vue
@@ -3,68 +3,67 @@
         <h1 class="text-5xl text-header font-bold font-roboto text-center">Menu masakan kami</h1>
         <div
             class="w-full border-b-[1px] pb-4 mt-10 gap-11 flex md:justify-center text-xl overflow-x-scroll md:overflow-hidden">
-            <button href="#"
-                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Breakfast</button>
-            <button href="#"
-                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Lunch</button>
-            <button href="#"
-                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Dinner</button>
-            <button href="#"
-                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Dessert</button>
+            <button @click="loadKepiting"
+                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Kepiting</button>
+            <button @click="loadLobster"
+                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Lobster</button>
+            <button @click="loadKerang"
+                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Kerang</button>
+            <button @click="loadIkan"
+                class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-17px] after:duration-300 focus:after:w-full hover:text-accent focus:text-accent cursor-pointer duration-150">Ikan</button>
         </div>
         <div class="w-full grid grid-cols-1 lg:grid-cols-2 pt-14 gap-8">
-            <div v-for="menu in menus" :key="menu"
+            <div v-for="food in foods" :key="food"
                 class="flex justify-between font-roboto font-bold text-2xl border-b-[1px] pb-4">
                 <div class="">
-                    <h2 class="text-header">{{ menu.name }}</h2>
-                    <p class="text-base font-normal mt-1 font-inter">{{ menu.desc }}</p>
+                    <h2 class="text-header">{{ food.name }}</h2>
+                    <p class="text-base font-normal mt-1 font-inter">{{ food.desc }}</p>
                 </div>
-                <span class="text-accent text-end">{{ menu.price }}</span>
+                <span class="text-accent text-end">{{ food.price }}</span>
             </div>
         </div>
         <div class="flex justify-center mt-8">
-            <a href="" class="border-[1px] border-accent text-accent py-2 px-6 rounded-md">View menu</a>
+            <button @click="load" class="border-[1px] border-accent text-accent py-2 px-6 rounded-md">View menu</button>
         </div>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    setup() {
-        let menus = [
-            {
-                name: 'Alder Grilled Chinook Salmon',
-                desc: 'Toasted French bread topped with romano, cheddar',
-                price: 'IDR 35K'
+    data() {
+        return {
+            foods: [],
+            lobster: [],
+        }
+    },
+    mounted() {
+        // this.load();
+    },
+    methods: {
+        async load() {
+            const response = await axios.get('http://localhost:3000/foods' + 'http://localhost:3000/lobster');
+            this.foods = response.data
+        },
+        async loadKepiting() {
+            console.log('kepiting')
+            const response = await axios.get('http://localhost:3000/foods');
+            this.foods = response.data
+        },
+        async loadLobster() {
+            console.log('lobster')
+            const response = await axios.get('http://localhost:3000/lobster');
+            this.foods = response.data
             },
-            {
-                name: 'King Crab',
-                desc: 'A 5 medium crab with black papper sauce',
-                price: 'IDR 35K'
+        async loadKerang() {
+            console.log('kerang')
+            const response = await axios.get('http://localhost:3000/kerang');
+            this.foods = response.data
             },
-            {
-                name: 'Lobster Party',
-                desc: 'Big sized lobster with padang sauce',
-                price: 'IDR 35K'
-            },
-            {
-                name: 'Mixed Seafood',
-                desc: 'Lobster, Crab, and Shell all in one',
-                price: 'IDR 35K'
-            },
-            {
-                name: 'Alder Grilled Chinook Salmon',
-                desc: 'Toasted French bread topped with romano, cheddar',
-                price: 'IDR 35K'
-            },
-            {
-                name: 'King Crab',
-                desc: 'A 5 medium crab with black papper sauce',
-                price: 'IDR 35K'
-            },
-        ]
+        },
 
-        return { menus }
+    setup() {
+        return {}
     }
 }
 </script>
